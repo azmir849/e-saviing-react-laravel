15d4a2ff821e7315df1dd89b3c380aa0039f5ac7
azmir849 <azmir15-849@diu.edu.bd>
2022-11-27 23:19:24 +0600

    ecom-laravel-app-initialized

---
+++ user/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import {Button} from "react-bootstrap";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Button variant="warning">Warning</Button>
+        <Button variant="info"> <i className="fa fa-home"></i> </Button>
+      </div>
+    )
+  }
+}
+
+export default App;
