b766592fc0006f6f2b5393deaca09f182e3dc069
azmir849 <azmir15-849@diu.edu.bd>
2022-11-28 00:11:47 +0600

    Nav Menu Desktop Part 1

--- user/src/components/common/NavMenuDesktop.jsx
+++ user/src/components/common/NavMenuDesktop.jsx
@@ -1,13 +1,32 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from "react";
+import { Navbar, Container, Row, Col } from "react-bootstrap";
+import Logo from "../../assets/images/easyshop.png";
+import { Link } from "react-router-dom";
+class NavMenuDesktop extends Component {
+  render() {
+    return (
+      <Fragment>
+        <div className='TopSectionDown'>
+          <Navbar fixed={"top"} className='navbar' bg='light'>
+            <Container fluid={"true"}>
+              <Row>
+                <Col lg={4} md={4} sm={12} xs={12}>
+                  <Link to='/'>
+                    {" "}
+                     <img alt="" className='nav-logo' src={Logo} />{" "}
+                  </Link>
+                </Col>
 
- class NavMenuDesktop extends Component {
-     render() {
-          return (
-               <div>
+                <Col lg={4} md={4} sm={12} xs={12}></Col>
 
-               </div>
-          )
-     }
+                <Col lg={4} md={4} sm={12} xs={12}></Col>
+              </Row>
+            </Container>
+          </Navbar>
+        </div>
+      </Fragment>
+    );
+  }
 }
 
-export default NavMenuDesktop
+export default NavMenuDesktop;
--- user/src/pages/HomePage.jsx
+++ user/src/pages/HomePage.jsx
@@ -1,4 +1,5 @@
 import React, { Component, Fragment } from 'react'
+import NavMenuDesktop from '../components/common/NavMenuDesktop'
 import Categories from '../components/home/Categories'
 import Collection from '../components/home/Collection'
 import FeaturedProducts from '../components/home/FeaturedProducts'
@@ -8,13 +9,14 @@ import NewArrival from '../components/home/NewArrival'
 export class HomePage extends Component {
      render() {
           return (
-               <Fragment>
-                   <HomeTop />
-                   <FeaturedProducts />
-                   <NewArrival />
-                   <Collection />
-                   <Categories />
-               </Fragment>
+            <Fragment> 
+                <NavMenuDesktop />       
+                <HomeTop />  
+                <FeaturedProducts />
+                <NewArrival />
+                <Categories />
+                <Collection />
+            </Fragment>
           )
      }
 }
