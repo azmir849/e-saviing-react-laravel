2523af2e76425d66f3a6e08ccb927516db1f08b9
azmir849 <azmir15-849@diu.edu.bd>
2022-11-28 00:01:33 +0600

    Accordion Menu Component Part 2

--- user/src/components/home/MegaMenu.jsx
+++ user/src/components/home/MegaMenu.jsx
@@ -1,12 +1,199 @@
 import React, { Component, Fragment } from "react";
 
 class MegaMenu extends Component {
+  constructor() {
+    super();
+    this.MegaMenu = this.MegaMenu.bind(this);
+  }
+
+  componentDidMount() {
+    this.MegaMenu();
+  }
+
+  MegaMenu() {
+    var acc = document.getElementsByClassName("accordion");
+    var accNum = acc.length;
+    var i;
+    for (i = 0; i < accNum; i++) {
+      acc[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+          panel.style.maxHeight = null;
+        } else {
+          panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+      });
+    }
+  }
+
   render() {
     return (
       <div className='accordionMenuDiv'>
         <div className='accordionMenuDivInside'>
           <button className='accordion'>
-           <img alt=""
+            <img alt=""
+              className='accordionMenuIcon'
+              src='https://image.flaticon.com/icons/png/128/739/739249.png'
+            />
+            &nbsp; Men's Clothing
+          </button>
+          <div className='panel'>
+            <ul>
+              <li>
+                <a href='/' className='accordionItem'>
+                  {" "}
+                  Mans Tshirt 1
+                </a>
+              </li>
+              <li>
+                <a href='/' className='accordionItem'>
+                  {" "}
+                  Mans Tshirt 2
+                </a>
+              </li>
+            </ul>
+          </div>
+          <button className='accordion'>
+            <img alt=""
+              className='accordionMenuIcon'
+              src='https://image.flaticon.com/icons/png/128/739/739249.png'
+            />
+            &nbsp; Men's Clothing
+          </button>
+          <div className='panel'>
+            <ul>
+              <li>
+                <a href='/' className='accordionItem'>
+                  {" "}
+                  Mans Tshirt 1
+                </a>
+              </li>
+              <li>
+                <a href='/' className='accordionItem'>
+                  {" "}
+                  Mans Tshirt 2
+                </a>
+              </li>
+            </ul>
+          </div>
+          <button className='accordion'>
+            <img alt=""
+              className='accordionMenuIcon'
+              src='https://image.flaticon.com/icons/png/128/739/739249.png'
+            />
+            &nbsp; Men's Clothing
+          </button>
+          <div className='panel'>
+            <ul>
+              <li>
+                <a href='/' className='accordionItem'>
+                  {" "}
+                  Mans Tshirt 1
+                </a>
+              </li>
+              <li>
+                <a href='/' className='accordionItem'>
+                  {" "}
+                  Mans Tshirt 2
+                </a>
+              </li>
+            </ul>
+          </div>
+          <button className='accordion'>
+            <img alt=""
+              className='accordionMenuIcon'
+              src='https://image.flaticon.com/icons/png/128/739/739249.png'
+            />
+            &nbsp; Men's Clothing
+          </button>
+          <div className='panel'>
+            <ul>
+              <li>
+                <a href='/' className='accordionItem'>
+                  {" "}
+                  Mans Tshirt 1
+                </a>
+              </li>
+              <li>
+                <a href='/' className='accordionItem'>
+                  {" "}
+                  Mans Tshirt 2
+                </a>
+              </li>
+            </ul>
+          </div>
+          <button className='accordion'>
+            <img alt=""
+              className='accordionMenuIcon'
+              src='https://image.flaticon.com/icons/png/128/739/739249.png'
+            />
+            &nbsp; Men's Clothing
+          </button>
+          <div className='panel'>
+            <ul>
+              <li>
+                <a href='/' className='accordionItem'>
+                  {" "}
+                  Mans Tshirt 1
+                </a>
+              </li>
+              <li>
+                <a href='/' className='accordionItem'>
+                  {" "}
+                  Mans Tshirt 2
+                </a>
+              </li>
+            </ul>
+          </div>
+          <button className='accordion'>
+            <img alt=""
+              className='accordionMenuIcon'
+              src='https://image.flaticon.com/icons/png/128/739/739249.png'
+            />
+            &nbsp; Men's Clothing
+          </button>
+          <div className='panel'>
+            <ul>
+              <li>
+                <a href='/' className='accordionItem'>
+                  {" "}
+                  Mans Tshirt 1
+                </a>
+              </li>
+              <li>
+                <a href='/' className='accordionItem'>
+                  {" "}
+                  Mans Tshirt 2
+                </a>
+              </li>
+            </ul>
+          </div>
+          <button className='accordion'>
+            <img alt=""
+              className='accordionMenuIcon'
+              src='https://image.flaticon.com/icons/png/128/739/739249.png'
+            />
+            &nbsp; Men's Clothing
+          </button>
+          <div className='panel'>
+            <ul>
+              <li>
+                <a href='/' className='accordionItem'>
+                  {" "}
+                  Mans Tshirt 1
+                </a>
+              </li>
+              <li>
+                <a href='/' className='accordionItem'>
+                  {" "}
+                  Mans Tshirt 2
+                </a>
+              </li>
+            </ul>
+          </div>
+          <button className='accordion'>
+            <img alt=""
               className='accordionMenuIcon'
               src='https://image.flaticon.com/icons/png/128/739/739249.png'
             />
@@ -30,7 +217,7 @@ class MegaMenu extends Component {
           </div>
 
           <button className='accordion'>
-           <img alt=""
+            <img alt=""
               className='accordionMenuIcon'
               src='https://image.flaticon.com/icons/png/128/739/739249.png'
             />
@@ -54,7 +241,7 @@ class MegaMenu extends Component {
           </div>
 
           <button className='accordion'>
-           <img alt=""
+            <img alt=""
               className='accordionMenuIcon'
               src='https://image.flaticon.com/icons/png/128/739/739249.png'
             />
@@ -78,7 +265,7 @@ class MegaMenu extends Component {
           </div>
 
           <button className='accordion'>
-           <img alt=""
+            <img alt=""
               className='accordionMenuIcon'
               src='https://image.flaticon.com/icons/png/128/739/739249.png'
             />
@@ -102,7 +289,7 @@ class MegaMenu extends Component {
           </div>
 
           <button className='accordion'>
-           <img alt=""
+            <img alt=""
               className='accordionMenuIcon'
               src='https://image.flaticon.com/icons/png/128/739/739249.png'
             />
@@ -126,7 +313,7 @@ class MegaMenu extends Component {
           </div>
 
           <button className='accordion'>
-           <img alt=""
+            <img alt=""
               className='accordionMenuIcon'
               src='https://image.flaticon.com/icons/png/128/739/739249.png'
             />
@@ -150,7 +337,7 @@ class MegaMenu extends Component {
           </div>
 
           <button className='accordion'>
-           <img alt=""
+            <img alt=""
               className='accordionMenuIcon'
               src='https://image.flaticon.com/icons/png/128/739/739249.png'
             />
@@ -174,7 +361,7 @@ class MegaMenu extends Component {
           </div>
 
           <button className='accordion'>
-           <img alt=""
+            <img alt=""
               className='accordionMenuIcon'
               src='https://image.flaticon.com/icons/png/128/739/739249.png'
             />
@@ -201,5 +388,4 @@ class MegaMenu extends Component {
     );
   }
 }
-
 export default MegaMenu;
