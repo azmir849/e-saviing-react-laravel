076bcb1f69b6d52474de65a1532d69b21997a41b
azmir849 <azmir15-849@diu.edu.bd>
2022-11-27 23:50:31 +0600

    New Arrival Slick Carousel Part 2

--- user/src/components/home/NewArrival.jsx
+++ user/src/components/home/NewArrival.jsx
@@ -5,10 +5,21 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 class NewArrival extends Component {
+  constructor(props) {
+    super(props);
+    this.next = this.next.bind(this);
+    this.previous = this.previous.bind(this);
+  }
+  next() {
+    this.slider.slickNext();
+  }
+  previous() {
+    this.slider.slickPrev();
+  }
   render() {
     var settings = {
       dots: false,
-      infinite: false,
+      infinite: true,
       speed: 500,
       autoplay: true,
       autoplaySpeed: 3000,
@@ -48,15 +59,26 @@ class NewArrival extends Component {
       <Fragment>
         <Container className='text-center' fluid={true}>
           <div className='section-title text-center mb-55'>
+            <h2>
+              NEW ARRIVAL &nbsp;
+              <a href="/" className='btn btn-sm ml-2 site-btn' onClick={this.previous}>
+                <i className='fa fa-angle-left'></i>
+              </a>
+              &nbsp;
+              <a href="/" className='btn btn-sm ml-2 site-btn' onClick={this.next}>
+                <i className='fa fa-angle-right'></i>
+              </a>
+            </h2>
             <h2>NEW ARRIVAL</h2>
             <p>Some Of Our Exclusive Collection, You May Like</p>
           </div>
 
           <Row>
-            <Slider {...settings}>
+            <Slider ref={(c) => (this.slider = c)} {...settings}>
               <div>
                 <Card className='image-box card'>
-                 <img alt=""
+                  <img
+                    alt=''
                     className='center'
                     src='https://rukminim1.flixcart.com/image/800/960/k7z3afk0/watch/t/c/x/lcs-8188-lois-caron-original-imafq3k9ztzdkyhe.jpeg?q=50'
                   />
@@ -70,7 +92,8 @@ class NewArrival extends Component {
               </div>
               <div>
                 <Card className='image-box card'>
-                 <img alt=""
+                  <img
+                    alt=''
                     className='center'
                     src='https://rukminim1.flixcart.com/image/800/960/jeka07k0/watch/4/p/y/38024pp25-fastrack-original-imaf37n5df3bn52n.jpeg?q=50'
                   />
@@ -84,7 +107,8 @@ class NewArrival extends Component {
               </div>
               <div>
                 <Card className='image-box card'>
-                 <img alt=""
+                  <img
+                    alt=''
                     className='center'
                     src='https://rukminim1.flixcart.com/image/800/960/kka1si80/watch/4/t/k/tw-02524-teenager-luxurious-fashion-silicone-black-colored-led-original-imafznht7bzfmj7d.jpeg?q=50'
                   />
@@ -98,7 +122,8 @@ class NewArrival extends Component {
               </div>
               <div>
                 <Card className='image-box card'>
-                 <img alt=""
+                  <img
+                    alt=''
                     className='center'
                     src='https://rukminim1.flixcart.com/image/800/960/jw0zr0w0/watch/c/u/r/ls2811-limestone-original-imafgrxqf8qvecjd.jpeg?q=50'
                   />
@@ -112,7 +137,8 @@ class NewArrival extends Component {
               </div>
               <div>
                 <Card className='image-box card'>
-                 <img alt=""
+                  <img
+                    alt=''
                     className='center'
                     src='https://rukminim1.flixcart.com/image/800/960/jcxoya80/watch/z/n/h/skmei-sports-multifunctional-dual-time-digital-blue-dial-men-s-original-imaffykneyfryvqh.jpeg?q=50'
                   />
@@ -126,7 +152,8 @@ class NewArrival extends Component {
               </div>
               <div>
                 <Card className='image-box card'>
-                 <img alt=""
+                  <img
+                    alt=''
                     className='center'
                     src='https://rukminim1.flixcart.com/image/800/960/kpodocw0/watch/p/l/t/anlg-428-blue-blu-analogue-original-imag3uxbhfkyhahf.jpeg?q=50'
                   />
@@ -140,7 +167,8 @@ class NewArrival extends Component {
               </div>
               <div>
                 <Card className='image-box card'>
-                 <img alt=""
+                  <img
+                    alt=''
                     className='center'
                     src='https://rukminim1.flixcart.com/image/800/960/k48rwcw0/watch/k/v/f/lcs-8190-lois-caron-original-imafn7fsyttnpybp.jpeg?q=50'
                   />
@@ -154,7 +182,8 @@ class NewArrival extends Component {
               </div>
               <div>
                 <Card className='image-box card'>
-                 <img alt=""
+                  <img
+                    alt=''
                     className='center'
                     src='https://rukminim1.flixcart.com/image/800/960/ke353m80-0/watch/e/b/s/fresh-new-arrival-latest-men-watch-watches-men-ghadi-gents-boys-original-imafuupqgaanhtxu.jpeg?q=50'
                   />
