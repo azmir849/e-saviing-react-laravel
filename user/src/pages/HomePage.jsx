428d7cb4aefe62fb5ebdbbf90def01962606895d
FNReact <commongithub@gmail.com>
2022-11-28 10:01:37 +0600

    Mobile View Menu Part 4

--- user/src/pages/HomePage.jsx
+++ user/src/pages/HomePage.jsx
@@ -1,25 +1,33 @@
-import React, { Component, Fragment } from 'react'
-import NavMenuDesktop from '../components/common/NavMenuDesktop'
-import NavMenuMobile from '../components/common/NavMenuMobile'
-import Categories from '../components/home/Categories'
-import Collection from '../components/home/Collection'
-import FeaturedProducts from '../components/home/FeaturedProducts'
-import HomeTop from '../components/home/HomeTop'
-import NewArrival from '../components/home/NewArrival'
+import React, { Component, Fragment } from "react";
+import NavMenuDesktop from "../components/common/NavMenuDesktop";
+import NavMenuMobile from "../components/common/NavMenuMobile";
+import Categories from "../components/home/Categories";
+import Collection from "../components/home/Collection";
+import FeaturedProducts from "../components/home/FeaturedProducts";
+import HomeTop from "../components/home/HomeTop";
+import HomeTopMobile from "../components/home/HomeTopMobile";
+import NewArrival from "../components/home/NewArrival";
 
 export class HomePage extends Component {
-     render() {
-          return (
-            <Fragment> 
-                <NavMenuMobile />    
-                <HomeTop />  
-                <FeaturedProducts />
-                <NewArrival />
-                <Categories />
-                <Collection />
-            </Fragment>
-          )
-     }
+  render() {
+    return (
+      <Fragment>
+        <div className="Desktop">
+          <NavMenuDesktop />
+          <HomeTop />
+        </div>
+
+        <div className="Mobile">
+          <NavMenuMobile />
+          <HomeTopMobile />
+        </div>
+        <FeaturedProducts />
+        <NewArrival />
+        <Categories />
+        <Collection />
+      </Fragment>
+    );
+  }
 }
 
-export default HomePage
+export default HomePage;
