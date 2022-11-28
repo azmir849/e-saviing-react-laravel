64e0b11c2e979b7c656425e16f99edc8c1c6398c
azmir849 <azmir15-849@diu.edu.bd>
2022-11-28 21:45:14 +0600

    Notification Page

--- user/src/components/common/NavMenuDesktop.jsx
+++ user/src/components/common/NavMenuDesktop.jsx
@@ -39,52 +39,54 @@ class NavMenuDesktop extends Component {
   render() {
     return (
       <Fragment>
-        <div className="TopSectionDown">
-          <Navbar fixed={"top"} className="navbar" bg="light">
+        <div className='TopSectionDown'>
+          <Navbar fixed={"top"} className='navbar' bg='light'>
             <Container
               fluid={"true"}
-              className="fixed-top shadow-sm p-2 mb-0 bg-white"
+              className='fixed-top shadow-sm p-2 mb-0 bg-white'
             >
               <Row>
                 <Col lg={4} md={4} sm={12} xs={12}>
-                  <Button onClick={this.MenuBarClickHandler} className="btn">
-                    <i className="fa fa-bars"></i>{" "}
+                  <Button onClick={this.MenuBarClickHandler} className='btn'>
+                    <i className='fa fa-bars'></i>{" "}
                   </Button>
-                  <Link to="/">
+                  <Link to='/'>
                     {" "}
-                    <img alt="" className="nav-logo" src={Logo} />{" "}
+                    <img alt='' className='nav-logo' src={Logo} />{" "}
                   </Link>
                 </Col>
 
-                <Col className="p-1 mt-1" lg={4} md={4} sm={12} xs={12}>
-                  <div className="input-group w-100">
-                    <input type="text" className="form-control" />
-                    <Button type="button" className="btn site-btn">
-                      <i className="fa fa-search"> </i>
+                <Col className='p-1 mt-1' lg={4} md={4} sm={12} xs={12}>
+                  <div className='input-group w-100'>
+                    <input type='text' className='form-control' />
+                    <Button type='button' className='btn site-btn'>
+                      <i className='fa fa-search'> </i>
                     </Button>
                   </div>
                 </Col>
 
-                <Col className="p-1 mt-1" lg={4} md={4} sm={12} xs={12}>
-                  <Link to="/" className="btn">
-                    <i className="fa h4 fa-heart"></i>
+                <Col className='p-1 mt-1' lg={4} md={4} sm={12} xs={12}>
+                  <Link to='/' className='btn'>
+                    <i className='fa h4 fa-heart'></i>
                     <sup>
-                      <span className="badge text-white bg-danger">3</span>
+                      <span className='badge text-white bg-danger'>3</span>
                     </sup>
                   </Link>
-                  <Link to="/" className="btn">
-                    <i className="fa h4 fa-bell"></i>
+                  <Link to='/notification' className='btn'>
+                    <i className='fa h4 fa-bell'></i>
                     <sup>
-                      <span className="badge text-white bg-danger">5</span>
+                      <span className='badge text-white bg-danger'>5</span>
                     </sup>
                   </Link>
-                  <a href="/" className="btn">
-                    <i className="fa h4 fa-mobile-alt"></i>
+                  <a href='/' className='btn'>
+                    <i className='fa h4 fa-mobile-alt'></i>
                   </a>
-                  <Link to="/login" className="h4 btn">LOGIN</Link>
+                  <Link to='/login' className='h4 btn'>
+                    LOGIN
+                  </Link>
 
-                  <Button className="cart-btn">
-                    <i className="fa fa-shopping-cart"></i> 3 Items{" "}
+                  <Button className='cart-btn'>
+                    <i className='fa fa-shopping-cart'></i> 3 Items{" "}
                   </Button>
                 </Col>
               </Row>
